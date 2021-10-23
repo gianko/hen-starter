92f5326c2b30b195485fd7f58874579b9ab07ed4
Nico Fonseca <nicolas.fonseca501@gmail.com>
2021-10-23 17:41:17 +0200

    Make container square only with CSS
    - Delete JavaScript resize function and OVERWRITE_ON_FULLSCREEN const
    - Use CSS clamp function on the container to make sure that it will be always a square based on the screen resolution

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,12 @@
-import debounce from 'lodash.debounce'
 import style from './style.module.css'
 
 // HEN User Data
 // const creator = new URLSearchParams(window.location.search).get('creator')
 // const viewer = new URLSearchParams(window.location.search).get('viewer')
 
-// set this to true if you want to override css' max-width variable on fullscreen
-const OVERWRITE_ON_FULLSCREEN = false
-
 const app = document.querySelector<HTMLDivElement>('#app')!
 app.className = style.app
 app.innerHTML = `
-  <div class="${style.wrapper}">
-    <div class="${style.container}">
-    </div>
+  <div class="${style.container}">
   </div>
 `
-
-const onResize = () => {
-  if (OVERWRITE_ON_FULLSCREEN) {
-    const { innerWidth, innerHeight } = window
-    window.document.documentElement.style.setProperty('--max-width', `${Math.min(innerHeight, innerWidth)}px`)
-  }
-}
-
-window.addEventListener('resize', debounce(onResize))
-onResize()
